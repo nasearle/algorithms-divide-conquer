4d00a5d6f9c72263385bb74c332f969ece5b45a2
Nicolas Searle <nsearle@google.com>
2017-10-15 13:46:54 -0700

    merge-sort

---
+++ week1/merge-sort.py
@@ -0,0 +1,35 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)/2
+        arr1 = arr[:mid]
+        arr2 = arr[mid:]
+        mergeSort(arr1)
+        mergeSort(arr2)
+        i = 0
+        j = 0
+        k = 0
+        output = []
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                arr[k] = arr1[i]
+                i += 1
+            else:
+                arr[k] = arr2[j]
+                j += 1
+            k += 1
+        while i < len(arr1):
+            arr[k] = arr1[i]
+            i += 1
+            k += 1
+        while j < len(arr2):
+            arr[k] = arr2[j]
+            j += 1
+            k += 1
+        print arr
+
+unsorted = [3, 2, 4, 1]
+
+actual = mergeSort(unsorted)
+expected = [1, 2, 3, 4]
+
+print actual == expected
